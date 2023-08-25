398e4fa304dc90d5ca4c8e5ce89b43da2bb3865f
trandangquang <trandangquang2203@gmail.com>
2023-08-25 11:45:52 +0700

    CRUD delete

--- src/controllers/homecontroller.js
+++ src/controllers/homecontroller.js
@@ -29,9 +29,6 @@ let postCRUD = async (req, res) => {
 
 let displayGetCRUD = async (req, res) => {
   let data = await CRUDService.getAllUser();
-  console.log('-------------------------------');
-  console.log(data);
-  console.log('-------------------------------');
   return res.render('displayCRUD.ejs', {
     dataTable: data
   });
@@ -58,6 +55,17 @@ let putCRUD = async (req, res) => {
   });
 }
 
+let deleteCRUD = async (req, res) => {
+  let id = req.query.id;
+  if (id){
+    await CRUDService.deleteUserById(id);
+    return res.send('delete user !!')
+  }else{
+    return res.send('User not found !!')
+  }
+
+}
+
 module.exports = {
   getHomePage: getHomePage,
   getAboutPage: getAboutPage,
@@ -66,4 +74,5 @@ module.exports = {
   displayGetCRUD: displayGetCRUD,
   getEditCRUD: getEditCRUD,
   putCRUD: putCRUD,
+  deleteCRUD: deleteCRUD,
 };
