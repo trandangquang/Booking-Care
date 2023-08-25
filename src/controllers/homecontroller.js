616536ce4c17aaa455aa6a39d5aa9bc5949bb53c
trandangquang <trandangquang2203@gmail.com>
2023-08-25 11:20:31 +0700

    CRUD edit

--- src/controllers/homecontroller.js
+++ src/controllers/homecontroller.js
@@ -37,10 +37,33 @@ let displayGetCRUD = async (req, res) => {
   });
 }
 
+let getEditCRUD = async(req, res) => {
+  let userId = req.query.id;
+  if (userId) {
+    let userData = await CRUDService.getUserInfoById(userId);
+
+    return res.render('editCRUD.ejs', {
+      user: userData
+    })
+  }else {
+    return res.send('user not found!')
+  }
+}
+
+let putCRUD = async (req, res) => {
+  let data = req.body;
+  let allUsers = await CRUDService.updateUserData(data);
+  return res.render('displayCRUD.ejs', {
+    dataTable: allUsers
+  });
+}
+
 module.exports = {
   getHomePage: getHomePage,
   getAboutPage: getAboutPage,
   getCRUD: getCRUD,
   postCRUD: postCRUD,
   displayGetCRUD: displayGetCRUD,
+  getEditCRUD: getEditCRUD,
+  putCRUD: putCRUD,
 };
